1d18275f3915f1fee8ddfee3ef2aaf2949bd2a7e
ericLimma <92587783+ericLimma@users.noreply.github.com>
2023-08-17 19:22:02 -0300

    update 17-08-23

--- src/components/layout/Projetos.jsx
+++ src/components/layout/Projetos.jsx
@@ -2,6 +2,7 @@ import styles from './Projetos.module.css'
 import { Card } from '../assets'
 
 import img_indecor from './img/indecor_header.jpg'
+import img_shopfree from './img/shopfree_header.jpg'
 
 
 export function Projetos() {
@@ -14,12 +15,12 @@ export function Projetos() {
                     <Card
                         img={img_indecor}
                         alt='descrição rapida da imagem'
-                        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                        description='Landing page de um evento de decoração.'
                         link='https://ericlimma.github.io/indecor/' />
                     <Card
-                        img={img_indecor}
+                        img={img_shopfree}
                         alt='descrição rapida da imagem'
-                        description='Site de demonstração inspirado na landing page da ShopFy'
+                        description='Site de demonstração inspirado na landing page da ShopFy.'
                         link='https://ericlimma.github.io/shopfree/' />
                     <Card
                         img={img_indecor}
@@ -27,6 +28,7 @@ export function Projetos() {
                         description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
                         link='#' />
                 </div>
+                <p className={styles.see_more}><a href="#">Ver mais ... </a></p>
             </section>
         </>
     )
